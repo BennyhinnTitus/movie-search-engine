4abab3fed16950e27411b12acf5d8a7978f6ae83
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-08 17:50:29 +0530

    Updated movie images

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,19 @@
 const movies = [
     {
         id: 1,
-        imageSrc: "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg",
+        imageSrc: "/avengers-endgame.jpg",
         title: "Avengers: Endgame",
         releaseDate: "2019-04-26"
     },
     {
         id: 2,
-        imageSrc: "https://m.media-amazon.com/images/I/51s+4QHL3yL._AC_SY679_.jpg",
+        imageSrc: "/inception.jpg",
         title: "Inception",
         releaseDate: "2010-07-16"
     },
     {
         id: 3,
-        imageSrc: "https://m.media-amazon.com/images/I/71V--WZVUIL._AC_SY679_.jpg",
+        imageSrc: "/batman.jpg",
         title: "The Batman",
         releaseDate: "2022-03-04"
     }
