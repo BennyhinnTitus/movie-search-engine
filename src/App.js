507708c6b78772cc5f40a2a4c013599c6afcdfa0
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-08-08 17:49:53 +0530

    Added page routing features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import Home from './components/Home';
+import Home from "./pages/Home";
+import Favorites from './pages/Favorites';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
     return (
         <div className="App">
-            <Home />
+            <NavBar />
+            <Routes>
+                <Route path='/' element={ <Home /> }/>
+                <Route path='/favorites' element={ <Favorites /> }/>
+            </Routes>
         </div>
     );
 }
